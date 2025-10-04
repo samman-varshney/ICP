70caeb1a6c43af0689caa8aafe3d3d08dedb471b
samman-varshney <sammanvarshney345@gmail.com>
2025-10-04 22:51:46 +0530

    added time and space complexity

--- assignment_7/Leetcode_62.java
+++ assignment_7/Leetcode_62.java
@@ -27,3 +27,16 @@ class Solution {
         return dp[0];
     }
 }
+
+
+//tc: O(nm)
+//sc: O(1)
+
+// Example 2:
+
+// Input: m = 3, n = 2
+// Output: 3
+// Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+// 1. Right -> Down -> Down
+// 2. Down -> Down -> Right
+// 3. Down -> Right -> Down
